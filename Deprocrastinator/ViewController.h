9df2f0b67c410be95e95f54334dc76a2f49ac079
Wade Sellers <Waderoni@gmail.com>
2014-10-06 14:08:08 -0500

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Wade Sellers on 10/6/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
