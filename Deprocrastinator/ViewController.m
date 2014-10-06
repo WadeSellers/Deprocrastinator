c0793bcd6cdfb8718329f065b73a953cec2c3852
Wade Sellers <Waderoni@gmail.com>
2014-10-06 16:53:37 -0500

    Can add items and check and uncheck any item

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property NSMutableArray *errands;
+@property (weak, nonatomic) IBOutlet UITextField *addItemField;
+@property (weak, nonatomic) IBOutlet UITableView *errandsTableView;
+@property NSIndexPath *lastIndex;
+@property NSMutableArray *checkedIndexPath;
 
 @end
 
@@ -16,13 +22,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.errands = [NSMutableArray arrayWithObjects:@"Take out trash",
+                    @"Do dishes",
+                    @"Clean room",
+                    @"Vacuum", nil];
+
+    //This is assigning an array of NO's for as many objects are in our errands array
+    self.checkedIndexPath = [NSMutableArray arrayWithCapacity:self.errands.count];
+    for (int i = 0; i < self.errands.count; i++)
+    {
+        [self.checkedIndexPath addObject:[NSNumber numberWithBool:NO]];
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//This tells my TableView how many rows it needs to build
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.errands.count;
 }
-//change
+
+// This tells what to put into each cell as it goes down the array, it will populate that many cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"toDoItems" forIndexPath:indexPath];
+
+    cell.textLabel.text = [self.errands objectAtIndex:indexPath.row];
+    return cell;
+}
+
+//When I tap the add button, add the object in the textfield to the array, empty the textfield, drop the keyboard, and reload the tableview so we see the new item.
+- (IBAction)onTappedAddButton:(id)sender
+{
+    [self.errands addObject:self.addItemField.text];
+    [self.checkedIndexPath addObject:[NSNumber numberWithBool:NO]];
+    self.addItemField.text = @"";
+    [self.addItemField resignFirstResponder];
+    [self.errandsTableView reloadData];
+}
+
+//When you select a cell, check to see if that cell has any accessories, if not give it 1.
+//If it does have an accessory, take it away.
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+
+    if (cell.accessoryType == UITableViewCellAccessoryNone)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        [self.checkedIndexPath replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:YES]];
+    }
+    else
+    {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        [self.checkedIndexPath replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:NO]];
+    }
+}
+
+
+
+
+
+
 
 @end
